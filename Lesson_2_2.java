e62e405fc9331c95deaf96b72c025445e5ac651f
Maksim <40897895+magihawa@users.noreply.github.com>
2026-08-12 06:41:47 +0300

    Модифицировал вывод заданий в sout

--- Lesson_2_2.java
+++ Lesson_2_2.java
@@ -1,28 +1,43 @@
 import java.util.Arrays;
 
 public class Lesson_2_2 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        System.out.println("Задание 1: ");
         printThreeWords();
+        System.out.println("\nЗадание 2: ");
         checkSumSign();
+        System.out.println("\nЗадание 3: ");
         printColor();
+        System.out.println("\nЗадание 4: ");
         compareNumbers();
-        checkSum(10, 11);
+        System.out.println("\nЗадание 5: ");
+        System.out.println(checkSum(10, 11));
+        System.out.println("\nЗадание 6: ");
         isNumberPositive(1);
-        isNumberPositiveBool(-1);
+        System.out.println("\nЗадание 7: ");
+        System.out.println(isNumberNegativeBool(-1));
+        System.out.println("\nЗадание 8: ");
         printStrings("Text", 5);
-        isLeapYear(400);
+        System.out.println("\nЗадание 9: ");
+        System.out.println(isLeapYear(400));
+        System.out.println("\nЗадание 10: ");
         invertArray();
+        System.out.println("\nЗадание 11: ");
         emptyArray();
+        System.out.println("\nЗадание 12: ");
         multiplyByTwo();
+        System.out.println("\nЗадание 13: ");
         diagonallyFill(5);
+        System.out.println("\nЗадание 14: ");
         System.out.println(Arrays.toString(returnArray(5, 1)));
 
 
     }
+
     /*
     1. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple
     */
-    public static void printThreeWords (){
+    public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
@@ -32,10 +47,10 @@ public class Lesson_2_2 {
     2. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями, которыми захотите. 
     Далее метод должен просуммировать эти переменные, и если их сумма больше или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
     */
-    public static void checkSumSign(){
-        int a = 1;
-        int b = 2;
-        if (a + b > 0){
+    public static void checkSumSign() {
+        int a = 0;
+        int b = 0;
+        if (a + b >= 0) {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
@@ -48,21 +63,22 @@ public class Lesson_2_2 {
     если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”, 
     если больше 100 (100 исключительно) - “Зеленый”;
     */
-    public static void printColor(){
+    public static void printColor() {
         int value = 101;
-        if (value <= 0)
+        if (value <= 0) {
             System.out.println("Красный");
-        else if (value <= 100)
+        } else if (value <= 100) {
             System.out.println("Желтый");
-        else
+        } else {
             System.out.println("Зеленый");
+        }
     }
 
     /*
     4. Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями, которыми захотите. 
     Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
     */
-    public static void compareNumbers(){
+    public static void compareNumbers() {
         int a = 3;
         int b = 2;
         if (a >= b) {
@@ -77,7 +93,7 @@ public class Lesson_2_2 {
     5. Напишите метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно),
     если да – вернуть true, в противном случае – false.
     */
-    public static boolean checkSum(int a, int b){
+    public static boolean checkSum(int a, int b) {
         int sum = a + b;
         return sum >= 10 && sum <= 20;
     }
@@ -86,45 +102,47 @@ public class Lesson_2_2 {
     6. Напишите метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, 
     положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
     */
-    public static void isNumberPositive (int a){
-        if (a >= 0)
+    public static void isNumberPositive(int a) {
+        if (a >= 0) {
             System.out.println("Число положительное");
-        else
+        } else {
             System.out.println("Число отрицательное");
+        }
     }
+
     /*
     7. Напишите метод, которому в качестве параметра передается целое число. 
     Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
     Замечание: ноль считаем положительным числом.
     */
-    public static boolean isNumberPositiveBool (int a){
+    public static boolean isNumberNegativeBool(int a) {
         return a < 0;
     }
 
     /*
     8. Напишите метод, которому в качестве аргументов передается строка и число, метод должен отпечатать в консоль указанную строку, указанное количество раз;
     */
-    public static void printStrings (String text, int a){
+    public static void printStrings(String text, int a) {
         for (int i = 0; i < a; i++) {
-        System.out.println(text);
+            System.out.println(text);
         }
     }
-    
+
     /*
     9. Напишите метод, который определяет, является ли год високосным, и возвращает boolean (високосный - true, не високосный - false). 
     Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
     */
-    public static boolean isLeapYear (int year){
-        return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 );
+    public static boolean isLeapYear(int year) {
+        return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
     }
 
     /*
     10. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. 
     С помощью цикла и условия заменить 0 на 1, 1 на 0;
     */
-    public static void invertArray (){
+    public static void invertArray() {
         int[] array = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = 1 - array[i];
         }
         System.out.println(Arrays.toString(array));
@@ -133,9 +151,9 @@ public class Lesson_2_2 {
     /*
     11. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100;
     */
-    public static void emptyArray (){
+    public static void emptyArray() {
         int[] array = new int[100];
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = i + 1;
         }
         System.out.println(Arrays.toString(array));
@@ -144,11 +162,11 @@ public class Lesson_2_2 {
     /*
     12. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
     */
-    public static void multiplyByTwo (){
+    public static void multiplyByTwo() {
         int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < array.length; i++){
-            if (array[i] < 6){
-            array[i] *= 2;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
             }
         }
         System.out.println(Arrays.toString(array));
@@ -159,10 +177,10 @@ public class Lesson_2_2 {
     и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно). 
     Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], ..., [n][n];
     */
-    public static void diagonallyFill (int size){
-        int[][] table = new int [size][size];
+    public static void diagonallyFill(int size) {
+        int[][] table = new int[size][size];
         final int FILL_VALUE = 1;
-        
+
         for (int i = 0; i < table.length; i++) {
             table[i][i] = FILL_VALUE;
             table[i][size - 1 - i] = FILL_VALUE; //Зеркальное отражение - индекс последнего столбца
@@ -177,11 +195,11 @@ public class Lesson_2_2 {
     14. Написать метод, принимающий на вход два аргумента: len и initialValue, 
     и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue.
     */
-    public static int[] returnArray (int len, int initialValue){
-        int [] array = new int [len];
-        for (int i = 0; i < array.length; i++){
+    public static int[] returnArray(int len, int initialValue) {
+        int[] array = new int[len];
+        for (int i = 0; i < array.length; i++) {
             array[i] = initialValue;
         }
-       return array;
+        return array;
     }
 }
